4ec9f15ea36c30213e6ee5df9b76eee5ac1dc579
majdouline <kalakhym@gmail.com>
2026-04-20 12:57:08 +0200

    Initial Commit

---
+++ PDFReader/PDFReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  PDFReaderApp.swift
+//  PDFReader
+//
+//  Created by Majdouline on 20/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct PDFReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
